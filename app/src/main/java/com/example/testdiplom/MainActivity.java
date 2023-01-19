b05009813e56e7a169fb102640f5270c9c1e5353
Quatros777 <ilya.kurduykoff@yandex.ru>
2023-01-19 23:18:58 +0300

    test commit1

--- app/src/main/java/com/example/testdiplom/MainActivity.java
+++ app/src/main/java/com/example/testdiplom/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.testdiplom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button ButtonVoiti;
+    private Button ButtonCreateAcc;
+    private Button ButtonVoitiFromAut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButtonVoiti = (Button) findViewById(R.id.ButtonVoiti);
+        ButtonVoiti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSingUpActivity();
+            }
+        });
+        }
+    public void openSingUpActivity() {
+        Intent intent = new Intent(this, SingUpActivity.class);
+        startActivity(intent);
     }
 }
