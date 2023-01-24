0595f6f23b10c6ab9cd013bfadd78e2852f83cc2
Quatros777 <ilya.kurdyukoff@yandex.ru>
2023-01-24 16:52:29 +0300

    commit5

--- app/src/main/java/com/example/testdiplom/profile.java
+++ app/src/main/java/com/example/testdiplom/profile.java
@@ -3,6 +3,12 @@ package com.example.testdiplom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class profile extends AppCompatActivity {
 
@@ -10,6 +16,23 @@ public class profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Button buttonMore = findViewById(R.id.moreinfo);
+        buttonMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(
+                        profile.this, R.style.BottomSheetDialogTheme
+                );
+                View bottomSheetView = LayoutInflater.from(getApplicationContext())
+                                .inflate(
+                                        R.layout.bottom_sheet,
+                                        (LinearLayout)findViewById(R.id.bottomSheetContainer)
+                                );
+                bottomSheetDialog.setContentView(bottomSheetView);
+                bottomSheetDialog.show();
+            }
+        });
     }
 
 }
