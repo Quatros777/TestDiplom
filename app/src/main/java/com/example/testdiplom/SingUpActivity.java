a2271b5f36d3d33d5e4b0c279259a395c9b8b212
Quatros777 <ilya.kurdyukoff@yandex.ru>
2023-01-25 17:35:20 +0300

    commitWithDB

--- app/src/main/java/com/example/testdiplom/SingUpActivity.java
+++ app/src/main/java/com/example/testdiplom/SingUpActivity.java
@@ -6,13 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.sql.CallableStatement;
 
 public class SingUpActivity extends AppCompatActivity {
+    //Конект к бд
+    private static connectionDB con=new connectionDB();
+
+    //Назначение кнопок
+    Button ButtonVoitiFromAut;
+    EditText emailFromVoiti, pswrdFromVoiti;
     private Button ButtonBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing_up);
+
+        //Определение кнопок
+        ButtonVoitiFromAut=findViewById(R.id.ButtonVoitiFromAut);
+        emailFromVoiti=findViewById(R.id.emailFromVoiti);
+        pswrdFromVoiti=findViewById(R.id.pswrdFromVoiti);
         ButtonBack = (Button) findViewById(R.id.ButtonBack);
         ButtonBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,5 +35,15 @@ public class SingUpActivity extends AppCompatActivity {
                 onBackPressed();
         }
     });
+    }
+
+    //Создание фунцкий или сессии (не совсем понял :))
+    public void Session(String user_email, String user_password){
+        try{
+            String UserInfoCall="{CALL users(?,?,?,?,?,?,?,?,?)}";
+//            CallableStatement cStmt=con.connexion()
+        }catch (Exception e){
+
+        }
     }
 }
