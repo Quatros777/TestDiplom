ab7478e65a510704c6511376441004c1b2866e6d
Quatros777 <ilya.kurdyukoff@yandex.ru>
2023-01-30 16:12:44 +0300

    commitWithDB2

---
+++ app/src/main/java/com/example/testdiplom/more_post.java
@@ -0,0 +1,34 @@
+package com.example.testdiplom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+public class more_post extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more_post);
+        Button showPhone = findViewById(R.id.showPhone);
+        showPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BottomSheetDialog BottomSheetPhone = new BottomSheetDialog(
+                        more_post.this, R.style.BottomSheetDialogTheme
+                );
+                View BottomSheetView2 = LayoutInflater.from(getApplicationContext())
+                        .inflate(
+                                R.layout.bottom_sheet_phone,
+                                (LinearLayout)findViewById(R.id.bottomSheetContainerPhone)
+                        );
+                BottomSheetPhone.setContentView(BottomSheetView2);
+                BottomSheetPhone.show();
+            }
+        });
+    }
+}
